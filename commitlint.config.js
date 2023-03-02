ef58faba2d849fccc9282401fde670f0f032ba4f
Pereira, Diego Amancio <diego.a.pereira@accenture.com>
2023-03-02 09:27:52 -0300

    config: add husky, commit lint and lint staged

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "type-enum": () => [
+            1,
+            "always",
+            [
+                "feature",
+                "fix",
+                "docs",
+                "refactor",
+                "test",
+                "revert",
+                "merge",
+                "style",
+                "build",
+                "config",
+            ],
+        ],
+        "scope-case": () => [0, "never"],
+        "subject-case": () => [0, "never"],
+    },
+};
